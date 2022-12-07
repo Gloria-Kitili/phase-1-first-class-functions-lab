7ec24b332ac2b713a7f55bbf049ca50056262d30
gloria kitili <gloria.kitili@student.moringaschool.com>
2022-12-07 13:05:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+// return first two drivers
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = () => drivers.slice(0,2)
+console.log(returnFirstTwoDrivers());
+
+
+const returnLastTwoDrivers =() => drivers.slice (-2)
+console.log(returnLastTwoDrivers());
+
+const selectingDrivers =
+[returnFirstTwoDrivers,returnLastTwoDrivers ];
+console.log(selectingDrivers);
+
+function createFareMultiplier(n){
+    return function(fare){
+    return n*fare 
+    }
+}
+console.log(createFareMultiplier(4))  
+
+const fareDoubler =(createFareMultiplier) => createFareMultiplier*2;
+fareDoubler(20);
+
+const fareTripler =(createFareMultiplier) => createFareMultiplier*3;
+fareTripler(20);
+
+
+const selectDifferentDrivers = (drivers,returnFirstTwoDrivers)=> returnFirstTwoDrivers(drivers);
